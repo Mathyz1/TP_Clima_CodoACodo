9159a7d45364695f36c1ae40fb1ec0992cb8eb99
Mathyz1 <49544505+Mathyz1@users.noreply.github.com>
2021-11-25 22:11:21 -0300

    Primer Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Navigation from './navigation/Navigation';
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      
+    </>
+  );
+};
+
+
+export default App;
